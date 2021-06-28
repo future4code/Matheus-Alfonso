5ae37ed162ac2e04f14fbd83474b5eb6748accf0
Matheus Alfonso <matheusalff@gmail.com>
2021-06-28 11:36:05 -0300

    Adicionado exercicio 1

---
+++ semana3/revisao-js/exercicios.js
@@ -0,0 +1,134 @@
+// EXERCÍCIO 01
+function inverteArray(array) {
+}
+
+// EXERCÍCIO 02
+function retornaNumerosParesElevadosADois(array) {
+  let arrayPares = []
+  let aoQuadrado
+  for(i = 0; i < array.length; i++){
+    if(array[i] % 2 === 0){
+      aoQuadrado = array[i] * array[i]
+      arrayPares.push(aoQuadrado)
+    }
+  }
+  return arrayPares
+}
+
+// EXERCÍCIO 03
+
+
+// EXERCÍCIO 04
+
+
+// EXERCÍCIO 05
+function retornaQuantidadeElementos(array) {
+
+}
+
+// EXERCÍCIO 06
+function retornaExpressoesBooleanas() {
+
+}
+
+// EXERCÍCIO 07
+function retornaNNumerosPares(n) {
+
+}
+
+// EXERCÍCIO 08
+function checaTriangulo(a, b, c) {
+  // return 'Escaleno'
+  // return 'Equilátero'
+  // return 'Isósceles'
+}
+
+// EXERCÍCIO 09
+function comparaDoisNumeros(num1, num2) {
+  // Formato do objeto a ser retornado:
+  // {
+  //   maiorNumero: X,
+  //   maiorDivisivelPorMenor: Y,
+  //   diferenca: Z
+  // }
+}
+
+// EXERCÍCIO 10
+function segundoMaiorEMenor(array) {
+
+}
+
+// EXERCÍCIO 11
+function ordenaArray(array) {
+
+}
+
+// EXERCÍCIO 12
+function filmeFavorito() {
+
+}
+
+// EXERCÍCIO 13
+function imprimeChamada() {
+  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+}
+
+// EXERCÍCIO 14
+function criaRetangulo(lado1, lado2) {
+
+}
+
+// EXERCÍCIO 15
+function anonimizaPessoa(pessoa) {
+
+}
+
+// EXERCÍCIO 16A
+function maioresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 16B
+function menoresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 17A
+function multiplicaArrayPor2(array) {
+
+}
+
+// EXERCÍCIO 17B
+function multiplicaArrayPor2S(array) {
+
+}
+
+// EXERCÍCIO 17C
+function verificaParidade(array) {
+
+}
+
+// EXERCÍCIO 18A
+function retornaPessoasAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 18B
+function retornaPessoasNaoAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 19A
+function ordenaPorNome(consultasNome) {
+
+}
+
+// EXERCÍCIO 19B
+function ordenaPorData(consultasData) {
+
+}
+
+// EXERCÍCIO 20
+function calculaSaldo(contas) {
+
+}
